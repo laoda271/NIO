2931b3ac27466c53740c895bf89739e7558df99b
chenminghe <chenminghe@jd.com>
2017-06-08 10:49:00 +0800

    缓冲区总结

--- aiguigu/src/main/java/_01TestBuffer.java
+++ aiguigu/src/main/java/_01TestBuffer.java
@@ -3,6 +3,43 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 
 /**
+ * 一、缓冲区(Buffer):在Java NIO 中负责数据的存取。缓冲区就是数组。用于存储不同类型的数据
+ *
+ * 根据类型的不同(Boolean除外），提供了相应类型的缓冲区：
+ * ByteBuffer
+ * CharBuffer
+ * ShortBuffer
+ * IntBuffer
+ * LongBuffer
+ * FloatBuffer
+ * DoubleBuffer
+ *
+ *
+ * 上述缓冲区的管理方式几乎一样，通过allocate()获取缓冲区
+ *
+ * 二、缓冲区存取数据的两个核心方法：
+ * put()：存入数据到缓冲区
+ * get()：获取缓冲区中的数据
+ *
+ * 三、缓冲区四个核心属性：
+ * capacity:容量，表示缓冲区中最大的存储数据的容量。一旦声明不能改变
+ * limit：界限，表示缓冲区中可以操作数据的大小（limit后数据不能进行读写）
+ * position：位置，表示缓冲区中正在操作数据的位置
+ *
+ * mark:标记，表示记录当前的position位置。可以通过reset恢复到mark位置
+ *
+ * 0 <= mark <= position <= limit <= capacity
+ *
+ * 四、直接缓冲区与非直接缓冲区
+ * 非直接缓冲区：通过allocate方法分配缓冲区，将缓冲区建立在JVM的内存中
+ * 直接缓冲区：通过allocateDirect方法分配直接缓冲区，将缓冲区建立在物理内存中，可以提高效率
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * Created by chenminghe on 2017/6/8.
  */
 public class _01TestBuffer {
