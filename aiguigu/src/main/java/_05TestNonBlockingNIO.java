ca79feb237ca02a0521129bc8c18d68f43a6d5bc
chenminghe <chenminghe@jd.com>
2017-06-17 18:35:57 +0800

    加入名称解释。

--- aiguigu/src/main/java/_05TestNonBlockingNIO.java
+++ aiguigu/src/main/java/_05TestNonBlockingNIO.java
@@ -9,6 +9,20 @@ import java.util.Scanner;
 
 /**
  * Created by chenminghe on 2017/6/14.
+ *  一、使用NIO完成网络的三个核心
+ *  1.通道(Channel):负责连接
+ *  java.nio.channels.Channel接口：
+ *      |-- SelectableChannel
+ *          |--SocketChannel
+ *          |--ServerSocketChannel
+ *          |--DatagramChannel
+ *
+ *          |--Pipe.SinkChannel
+ *          |--Pipe.SourceChannel
+ *  2.缓冲区(Buffer):负责数据的存取
+ *
+ *  3.选择器(Selector):是SelectableChannel的多路复用器。用于监控SelectableChannel的IO状况
+ *
  */
 public class _05TestNonBlockingNIO {
 
