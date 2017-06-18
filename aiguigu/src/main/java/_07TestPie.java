a97ee6db84080e71c3e4a7baf18d78bf00b6dd67
chenminghe <chenminghe@jd.com>
2017-06-18 15:21:28 +0800

    Pipe管道发送和读取数据

---
+++ aiguigu/src/main/java/_07TestPie.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+
+/**
+ * 管道写入和读取数据
+ *
+ * Flume框架的演化
+ * Created by chenminghe on 2017/6/18.
+ */
+public class _07TestPie {
+
+    @Test
+    public void test() throws Exception {
+        // 1.获取管道
+        Pipe pipe = Pipe.open();
+
+        // 2.将缓冲区的数据写入管道
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+
+        Pipe.SinkChannel sinkChannel = pipe.sink();
+        buf.put(new String("通过单向管道写数据").getBytes());
+        buf.flip();
+        sinkChannel.write(buf);
+
+        // 3.读取缓冲区中的数据
+        Pipe.SourceChannel sourceChannel = pipe.source();
+        ByteBuffer buf2 = ByteBuffer.allocate(1024);
+        // 4.读取缓冲区中的数据
+        int len = sourceChannel.read(buf2);
+        buf2.flip();
+        System.out.println(new String(buf2.array(),0,len));
+
+        sinkChannel.close();
+        sourceChannel.close();
+
+    }
+}
