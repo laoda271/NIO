70ade7014a08acec932d2fc0368bc71d4908ffdd
chenminghe <chenminghe@jd.com>
2017-06-18 14:59:35 +0800

    DatagramChannel发送数据

--- aiguigu/src/main/java/StartServer.java
+++ aiguigu/src/main/java/StartServer.java
@@ -5,7 +5,7 @@ import java.io.IOException;
  */
 public class StartServer {
     public static void main(String[] args) throws Exception{
-        new _05TestNonBlockingNIO().server();
+        //new _05TestNonBlockingNIO().server();
         new _06TestNonBlockingNIO2().receive();
     }
 }
